df549eeae68e4905e29dee6544e42ca15fe10bfb
valekar <srinivas.valekar@gmail.com>
2013-07-24 22:26:06 +0530

    user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+
+  def show
+    # this is used to show profile
+
+    @user = User.find(params[:id])
+  end
+
   def new
+  end
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Prototype::Application.routes.draw do
    # get "users/new"
+
+  resources :users
+
       get "static_pages/home"
       #get "static_pages/help"
       #get "static_pages/about"
