76c2f9caad92512ca8760f9364112ff85dce8f22
valekar <srinivas.valekar@gmail.com>
2013-07-31 21:12:12 +0530

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
   Prototype::Application.routes.draw do
    # get "users/new"
 
-  resources :users
+    resources :users do
+      member do
+        get :following, :followers
+      end
+    end
   resources :sessions,   only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   get "static_pages/home"
       #get "static_pages/help"
